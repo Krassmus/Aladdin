7fe6efc49889134fd52a6b8898970beea6666dfd
Rasmus Fuhse <fuhse@data-quest.de>
2016-02-01 13:08:18 +0100

    port 661b227c03d8a4de8a1dab09294f0289bdddfe21

--- views/lamp/_subbrainstorms.php
+++ views/lamp/_subbrainstorms.php
@@ -1,6 +1,6 @@
 <div class="subbrainstorms" data-brainstorm_id="<?= $brainstorm->getId() ?>" data-lasttime="<?= time() ?>">
     <? if ($brainstorm->children) : ?>
-        <? foreach ($brainstorm->children->orderBy('power DESC, mkdate ASC') as $child): ?>
+        <? foreach ($brainstorm->children->orderBy('power DESC, mkdate ASC', SORT_NUMERIC) as $child): ?>
             <?= $this->render_partial('lamp/_voteable_brainstorm', array('brainstorm' => $child)) ?>
         <? endforeach; ?>
     <? endif ?>
