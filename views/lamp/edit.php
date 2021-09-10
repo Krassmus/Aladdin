894d4a3c953fe8d8f688d3e7c76cbfa197639296
Rasmus Fuhse <fuhse@data-quest.de>
2021-09-10 13:28:20 +0200

    add migration

--- views/lamp/edit.php
+++ views/lamp/edit.php
@@ -3,21 +3,44 @@
       action="<?= $controller->url_for('lamp/edit/'.$brainstorm->getId()) ?>">
 
     <?= CSRFProtection::tokenTag() ?>
-    
+
     <input type="hidden" name="brainstorm[range_id]" value="<?= $range_id ?: $brainstorm['range_id'] ?>">
 
-    <? if (!$brainstorm['range_id']) : ?>
-    <label>
-        <?= dgettext("aladdin",'Titel') ?>
-        <input type="text" name="brainstorm[title]" value="<?= htmlReady($brainstorm['title']) ?>" placeholder="<?= dgettext("aladdin","Frage oder Thema") ?>">
-    </label>
-    <? endif ?>
+    <fieldset>
+        <legend>
+            <?= _("Fragestellung") ?>
+        </legend>
+        <? if (!$brainstorm['range_id']) : ?>
+            <label>
+                <?= dgettext("aladdin",'Titel') ?>
+                <input type="text"
+                       name="brainstorm[title]"
+                       value="<?= htmlReady($brainstorm['title']) ?>"
+                       placeholder="<?= dgettext("aladdin","Frage oder Thema") ?>">
+            </label>
+        <? endif ?>
 
-    <label>
-        <?= dgettext("aladdin",'Text') ?>
-        <textarea name="brainstorm[text]" placeholder="<?= dgettext("aladdin","Um was soll's gehen?") ?>"><?= htmlReady($brainstorm['text']) ?></textarea>
-    </label>
-    
+        <label>
+            <?= dgettext("aladdin",'Text') ?>
+            <textarea name="brainstorm[text]"
+                      placeholder="<?= dgettext("aladdin","Um was soll's gehen?") ?>"><?= htmlReady($brainstorm['text']) ?></textarea>
+        </label>
+    </fieldset>
+
+    <fieldset>
+        <legend>
+            <?= _("Metadaten") ?>
+        </legend>
+        <div class="hgroup">
+            <label>
+                <?= _("Startet am") ?>
+                <input type="text"
+                       name="brainstorm[start]"
+                       value="<?= $brainstorm['start'] ? date("d.m.Y H:i", $brainstorm['start']) : "" ?>">
+            </label>
+        </div>
+    </fieldset>
+
     <div data-dialog-button>
         <? if ($brainstorm->isNew()): ?>
             <?= \Studip\Button::create(dgettext("aladdin",'Anlegen'), 'create') ?>
